658ed80c29a48e25fd2453bc1be670df8c2082f6
YeongWoooo <rdd9223@gmail.com>
2020-11-21 18:22:59 +0900

    Feat: Account CRUD API 구현

---
+++ controllers/account_controller.js
@@ -0,0 +1,102 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "dbproject",
+  dateStrings: "date",
+});
+
+connection.connect();
+
+const getAccountPage = (req, res, next) => {
+  res.render("pages/main", {
+    user: "testUser",
+    photo: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+    page: "account",
+  });
+};
+
+// CREATE(ADD) 요청이 발생할 때, 요청자의 id와 accountid를 같이 insert 해주자.
+const addAccount = async (req, res) => {
+  let {
+    account_date,
+    account_name,
+    account_phone,
+    account_email,
+    account_balance,
+    account_type,
+    customer_idx,
+    card_requested,
+  } = req.body;
+  await connection.query(
+    `INSERT INTO account( account_date, account_name, account_phone, account_email, account_balance, account_type, customer_idx, card_requested) 
+    VALUES("${account_date}","${account_name}","${account_phone}","${account_email}","${account_balance}","${account_type}","${customer_idx}", "${card_requested}")`
+  );
+
+  readAllAccount(req, res);
+};
+
+// READ ALL
+const readAllAccount = (req, res) => {
+  connection.query("select * from account", (error, results, fields) => {
+    if (error) res.send(error);
+    else {
+      console.log(`${JSON.stringify(results)} data received!`);
+      res.send(JSON.stringify(results));
+    }
+  });
+};
+
+// READ
+const readAccount = (req, res) => {
+  const { idx } = req.params;
+  connection.query(`select * from account WHERE account_idx=${idx}`, (error, results, fields) => {
+    if (error) res.send(error);
+    else {
+      console.log(`${JSON.stringify(results)} data received!`);
+      res.send(JSON.stringify(results));
+    }
+  });
+};
+
+// UPDATE
+const updateAccount = (req, res) => {
+  const { idx } = req.params;
+  let {
+    account_date,
+    account_name,
+    account_phone,
+    account_email,
+    account_balance,
+    account_type,
+    customer_idx,
+    card_requested,
+  } = req.body;
+
+  connection.query(
+    `UPDATE account 
+    SET account_date = "${account_date}", account_name = "${account_name}", account_phone = "${account_phone}", 
+      account_email = "${account_email}", account_balance = "${account_balance}", account_type = "${account_type}",
+      customer_idx = "${customer_idx}", card_requested = "${card_requested}"
+    WHERE account_idx = "${idx}"`
+  );
+  readAllAccount(req, res);
+};
+
+// DELETE
+const deleteAccount = (req, res) => {
+  const { idx } = req.params;
+
+  connection.query(`DELETE FROM account WHERE account_idx = "${idx}"`);
+  readAllAccount(req, res);
+};
+
+module.exports = {
+  getAccountPage,
+  addAccount,
+  readAllAccount,
+  readAccount,
+  updateAccount,
+  deleteAccount,
+};
---
+++ routes/account_route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const accountController = require("../controllers/account_controller");
+
+// start with /plan
+// uuid를 사용하면 고유한 ID를 만들 수 있다. DB ID로 사용하면 유용할 듯
+// route middleware 함수는 controller에 보낸다.
+router.get("/page", accountController.getAccountPage);
+
+router.post("/", accountController.addAccount);
+
+// 전체 조회
+router.get("/", accountController.readAllAccount);
+// 1개 조회
+router.get("/:idx", accountController.readAccount);
+
+router.put("/:idx", accountController.updateAccount);
+
+// delete
+router.delete("/:idx", accountController.deleteAccount);
+
+module.exports = router;
