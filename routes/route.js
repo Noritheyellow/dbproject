ce611e50e92c08138c04dd89ffd0be4cd9be8a60
meohyun2 <eogus6242@gmail.com>
2020-11-22 22:25:32 +0900

    <feat> 올해, 작년 계좌 개설 수 확인

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const homeFunctions = require('../controllers/route.js');
+const fetch = require('node-fetch');
 
-router.get('/', homeFunctions.getHomepage)
+
+router.get("/",function(req,res){
+  let countThisYearDeal;
+  let countLastYearDeal;
+
+  fetch('http://localhost:8080/dealing/year?year=2020')
+  .then(res => res.json())
+  .then(json => countThisYearDeal = json.length)
+  .then(()=>{
+    fetch('http://localhost:8080/dealing/year?year=2019')
+    .then(res=> res.json())
+    .then(json => countLastYearDeal = json.length)
+    .then(()=>{
+      res.render("pages/main",{
+        user: "testUser",
+        photo: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        page: "home", 
+        data: {
+          countThisYearDeal: countThisYearDeal || 0,
+          countLastYearDeal: countLastYearDeal || 0
+        },
+      })
+    })
+    .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
+});
 
 router.post('/read', homeFunctions.postHomepage)
 
